d6606bb025c2142c158f150e09093c6e0360d0f2
developerfahad1 <fahadgraphic.pk>
2024-03-19 16:03:28 +0500

    app.js file and index.html file changed

--- app.js
+++ app.js
@@ -44,3 +44,48 @@
 // }while (i < 10)
 
 
+// let fruit = ['apple' , 'orange' , 'mango' , 'bannana' , 'grapes']
+
+// let ol = document.querySelector('ol');
+// function underloop(){
+//     ol.innerHTML = ''
+//     for(let i = 0; i < fruit.length; i++){
+//         console.log(fruit[i] , i);
+//         ol.innerHTML += `<li>${fruit[i]}</li>`
+//     }
+// }
+
+let fruits = [
+    "Apple", "Banana", "Orange", "Grapes", "Strawberry",
+    "Pineapple", "Mango", "Watermelon", "Kiwi", "Peach",
+    "Pear", "Cherry", "Plum", "Blueberry", "Raspberry",
+    "Lemon", "Lime", "Coconut", "Apricot", "Fig"
+];
+
+// // console.log(fruits);
+// let ol = document.querySelector('ol');
+// function underloop(){
+//     ol.innerHTML = ''
+//     for (let i = 0; i < fruits.length; i++) {
+//         console.log(fruits[i]);
+//     ol.innerHTML +=  `<li>${fruits[i]}</li>`
+//     }
+// }
+
+
+// do while loop 
+
+let i = 11;
+do {
+    console.log(i)
+    i++
+}while(i < 10);
+
+
+// while loop 
+
+let j = 1;
+while (j > 0){
+    console.log(j);
+    j++
+}
