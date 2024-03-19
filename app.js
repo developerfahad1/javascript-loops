8a3c7db5ce308d1e83ab0532299124741766e267
developerfahad1 <fahadgraphic.pk>
2024-03-19 15:13:04 +0500

    app.js and index.html file created

---
+++ app.js
@@ -0,0 +1,46 @@
+// console.log('hello world');
+
+// forloop
+// while loop
+// do while loop
+
+
+// for(let i = 0; i < 10; i++){
+//     console.log('hello world');
+// }
+
+
+
+// for(let i = 1; 1 < 10; i++){
+//     console.log('i');
+// }
+
+
+// loops class started 
+
+// let fruit = ['apple' , 'orange' , 'mango' , 'bannana' , 'grapes']
+// let ol = document.querySelector('ol')
+// function underloop(){
+//     for(let i = 0; i < fruit.length; i++){
+//     console.log(fruit[i] , i);
+//     ol.innerHTML += `<li>${fruit[i]}</li>`
+// }
+
+// }
+
+// function underloop(){
+//     let i = 0;
+//     while(i < fruit.length){
+//         ol.innerHTML += `<li>${fruit[i]}</li>`
+//     }
+// }
+
+
+// do while loop
+// let i = 11;
+// do {
+//     console.log('do while loop ===>' , i)
+//     i++
+// }while (i < 10)
+
+
